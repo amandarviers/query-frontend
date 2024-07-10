abecc312f3f8c5533764374da73a4d3e14a37b0a
Amanda Viers <amandaviers@Amandas-MacBook-Pro.local>
2024-07-10 08:05:58 -0500

    feat: use emailjs to add functionality to the contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,41 @@
 import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
 const ContactForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
 
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    console.log("Form submitted:", { name, email, message });
+    console.log({
+      from_name: name,
+      reply_to: email,
+      message: message,
+    });
+
+    emailjs
+      .sendForm(
+        "service_l6fo3n9",
+        "template_sufi43v",
+        e.target,
+        "COkfPBIn0dvihE9ag" // Replace with your actual EmailJS user ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setStatusMessage("Thank you for contact us!");
+          setName("");
+          setEmail("");
+          setMessage("");
+        },
+        (error) => {
+          console.log(error.text);
+          setStatusMessage("Error occurred. Please try again.");
+        }
+      );
   };
 
   return (
@@ -16,14 +43,17 @@ const ContactForm = () => {
       <h1>
         <span>Contact Us</span>
       </h1>
+      <p align="center">{statusMessage && <p>{statusMessage}</p>}</p>
       <div className="contactForm">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={sendEmail}>
           <label>
             Name
             <input
               type="text"
               value={name}
+              name="from_name"
               onChange={(e) => setName(e.target.value)}
+              required
             />
           </label>
           <br />
@@ -32,7 +62,9 @@ const ContactForm = () => {
             <input
               type="email"
               value={email}
+              name="reply_to"
               onChange={(e) => setEmail(e.target.value)}
+              required
             />
           </label>
           <br />
@@ -40,7 +72,9 @@ const ContactForm = () => {
             Message <br />
             <textarea
               value={message}
+              name="message"
               onChange={(e) => setMessage(e.target.value)}
+              required
             />
           </label>
           <br />
